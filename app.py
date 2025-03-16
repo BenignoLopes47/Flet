a866647b8631510bfad14ef018109131431dc535
BenignoLopes47 <benigno.lopes@sapo.pt>
2025-03-16 11:35:24 +0000

    nova alteração

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import flet as ft
 def main(page: ft.Page):
     page.title = "Hello world!"
-    page.add(ft.Text("Hello World!"))   
+    page.add(ft.Text("Hello World!")) 
+
+    def button_click(e):
+        page.add(ft.Text("Hello World!"))
+
+    page.add(ft.ElevatedButton("Click me", on_click=button_click))  
 ft.app(target=main)
