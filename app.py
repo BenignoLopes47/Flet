bebc1a0241952364d20a2a5447528f48e356ebcc
BenignoLopes47 <benigno.lopes@sapo.pt>
2025-03-26 12:33:35 +0000

    inicio

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import flet as ft
+
 def main(page: ft.Page):
-    page.title = "Hello"
-    page.add(ft.Text("Hello World!")) 
+    page.title = "Flet Counter"
+    page.add(ft.Text("Hello, World!"))  
 
-    def button_click(e):
-        page.add(ft.Text("Hello World!"))
-
-    page.add(ft.ElevatedButton("Click me", on_click=button_click))  
 ft.app(target=main)
